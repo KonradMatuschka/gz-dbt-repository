1310880be5ded38f4c40d5325cbd1675d6ad89f6
konradmatuschka@gmail.com <konradmatuschka@gmail.com>
2025-07-01 10:39:12 +0000

    cleaning raw data table

---
+++ models/staging/gz_raw_data/stg_raw_ship.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        logcost as log_cost, 
+        ship_cost
+
+    from source
+
+)
+
+select * from renamed
+
