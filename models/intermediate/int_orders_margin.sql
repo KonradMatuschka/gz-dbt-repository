9bdd3e53645acfb7ae73dbaf863506a884e67eef
konradmatuschka@gmail.com <konradmatuschka@gmail.com>
2025-07-01 13:32:01 +0000

    commit

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,10 +1,10 @@
 select
     int_sales_margin.orders_id,
     int_sales_margin.date_date,
-    ROUND(SUM(int_sales_margin.revenue),2) as revenue,
-    ROUND(SUM(int_sales_margin.quantity),2) as quantity,
-    ROUND(SUM(int_sales_margin.purchase_cost),2) as purchase_cost,
-    ROUND(SUM(int_sales_margin.margin),2) as margin  
+    cast(ROUND(SUM(int_sales_margin.revenue),2) as FLOAT64) as revenue,
+    cast(ROUND(SUM(int_sales_margin.quantity),2) as FLOAT64) as quantity,
+    cast(ROUND(SUM(int_sales_margin.purchase_cost),2) as FLOAT64) as purchase_cost,
+    cast(ROUND(SUM(int_sales_margin.margin),2) as FLOAT64) as margin  
 from {{ref("int_sales_margin")}} as int_sales_margin
 GROUP BY
     int_sales_margin.orders_id,
